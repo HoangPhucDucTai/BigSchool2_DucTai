18140efc99c98a2eff2e752a2dcbf50735d7f6a3
Administrator <Administrator@DUCTAIPC>
2022-06-21 18:35:54 +0700

    Add project files.

---
+++ BigSchool2_DucTai/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool2_DucTai.MvcApplication" Language="C#" %>
---
+++ BigSchool2_DucTai/ViewModels/CourseViewModels.cs
@@ -0,0 +1,35 @@
+﻿using BigSchool2_DucTai.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool2_DucTai.ViewModels
+{
+    public class CourseViewModels
+    {
+        [Required]
+        public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Category { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+
+    }
+}
